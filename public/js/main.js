ce2f170770c5e650c5a2fd0e1410e96d5c4a5076
Moises Sanabria <sesmoi.data@gmail.com>
2014-07-18 10:31:46 -0400

    adding showSadTweets function

--- public/js/main.js
+++ public/js/main.js
@@ -33,6 +33,30 @@
 				methods.getSadTweets('input');
 			});
 		},
+		showSadTweets: function () {
+      		var tweets 		= $('.tweet'),
+				firstTweet 	= tweets.first();        	
+
+			fadeFunction(firstTweet);
+
+			function fadeFunction(tweetElement) {
+				tweetElement.fadeIn( 1000, function() {
+					$(this).transition({scale: 1.06}, 5000);
+					$(this).fadeOut( 1000, function () {
+						if (tweetElement.next().length) {
+								fadeFunction(tweetElement.next());
+						} else {
+							console.log('no more sad tweets!');
+							$('.fin').fadeIn(1000, function(){
+								// $('this').load(function(){
+								// 	window.location.href = "http://prty.jp"
+								// });
+							});
+						}
+					});
+				});
+			}
+       	},
 		getSadTweets: function (context) {
 			
 			var params = {
