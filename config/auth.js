7d4925211186e35784b7e1f8cbe5fa25126e353c
Moises Sanabria <sesmoi.data@gmail.com>
2014-08-01 15:24:03 -0400

    changed url

--- config/auth.js
+++ config/auth.js
@@ -5,6 +5,6 @@ module.exports = {
 	'twitterAuth' : {
 		'consumerKey' 		: 'PKhzF4Ww6DqbqribSUxQGn5VG',
 		'consumerSecret' 	: 'aQuQHE827XmXmw4gz3NjXTB44LJ4o60gFhkBiB9S9eny2TJH4A',
-		'callbackURL' 		: 'http://localhost:3000/auth/twitter/callback'
+		'callbackURL' 		: 'http://sadtweets.com/auth/twitter/callback'
 	}
 };
